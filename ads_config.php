be875f86447069e5e795a8297a593699620b3292
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-01-04 17:40:49 +0530

    Create ads_config.php

---
+++ ads_config.php
@@ -0,0 +1,42 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "\/21753324030,22723853531\/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Banner",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "\/21753324030,22723853531\/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Interstitial",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "\/21753324030,22723853531\/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Native",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "\/21753324030,22723853531\/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Rewarded",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "\/21753324030,22723853531\/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_AppOpen",
+      "priority": "1"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": 50,
+      "is_show": "yes"
+    }
+  ]
+}
