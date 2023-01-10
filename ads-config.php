da99a68b70d0ea69a3da43875974f52302b548ee
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-01-10 17:35:47 +0530

    Create ads-config.php

---
+++ ads-config.php
@@ -0,0 +1,42 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "ca-app-pub-3940256099942544/6300978111",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "ca-app-pub-3940256099942544/1033173712",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "ca-app-pub-3940256099942544/2247696110",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "ca-app-pub-3940256099942544/5354046379",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "ca-app-pub-3940256099942544/3419835294",
+      "priority": "0"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": 50,
+      "is_show": "yes"
+    }
+  ]
+}
