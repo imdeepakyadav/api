44889053584a0b5bd454e4acc858ece3797506bf
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-02-04 02:53:41 +0530

    Update sp_dating_ads.php

--- videos/sp_dating_ads.php
+++ videos/sp_dating_ads.php
@@ -4,32 +4,32 @@
       "ad_network": "Admob",
       "unit_type": "Banner",
       "placement_id": "ca-app-pub-3940256099942544/6300978111",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Interstitial",
       "placement_id": "ca-app-pub-3940256099942544/1033173712",
-      "priority": "1",
+      "priority": "0",
       "counter": "5"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Native",
       "placement_id": "ca-app-pub-3940256099942544/2247696110",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Admob",
       "unit_type": "RewardAd",
       "placement_id": "ca-app-pub-3940256099942544/5354046379",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Admob",
       "unit_type": "AppOpenAd",
       "placement_id": "ca-app-pub-3940256099942544/3419835294",
-      "priority": "1"
+      "priority": "0"
     }
   ],
   "info": [
