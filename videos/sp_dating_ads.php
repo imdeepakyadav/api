c7882b9193ed2436f31fa568f91936f2787dd9a7
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-04-05 13:16:03 +0530

    Create sp_dating_ads.php

---
+++ videos/sp_dating_ads.php
@@ -0,0 +1,42 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "/21753324030,22810725888/com.girlslivevideocalling.freelivevideochat.randomvideocall.app_Banner",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "/21753324030,22810725888/com.girlslivevideocalling.freelivevideochat.randomvideocall.app_Interstitial",
+      "priority": "1",
+      "counter": "2"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "/21753324030,22810725888/com.girlslivevideocalling.freelivevideochat.randomvideocall.app_Native",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "/21753324030,22810725888/com.girlslivevideocalling.freelivevideochat.randomvideocall.app_Rewarded",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "/21753324030,22810725888/com.girlslivevideocalling.freelivevideochat.randomvideocall.app_AppOpen",
+      "priority": "1"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": "50",
+      "is_show": "yes"
+    }
+  ]
+}
