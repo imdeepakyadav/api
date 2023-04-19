c7ab372be9003c80e7907986751fdaa98c47411c
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-04-19 12:40:34 +0530

    Create test-ads.php

---
+++ test-ads.php
@@ -0,0 +1,69 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "ca-app-pub-3940256099942544/6300978111",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "ca-app-pub-3940256099942544/1033173712",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "ca-app-pub-3940256099942544/2247696110",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "ca-app-pub-3940256099942544/5224354917",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "ca-app-pub-3940256099942544/3419835294",
+      "priority": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Banner",
+      "placement_id": "1bd61e010479c716",
+      "priority": "0"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Interstitial",
+      "placement_id": "197f5ba608c27d44",
+      "priority": "0",
+      "counter": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Native",
+      "placement_id": "697c0a91e00756ca",
+      "priority": "0"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": 50,
+      "is_show": "yes"
+    }
+  ],
+"webrtc": [
+    {
+      "provider": "agora",
+      "app_id": "4d4b31da413f42e08394ab03b9f61938",
+      "certificate": "04b27a3359e24c3d9b5152fe8515a2b0",
+      "channel": "randomvideocall"
+    }
+  ]
+}
