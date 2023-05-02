a9814f8dae0e4724f4388f82b34669d2c711c44c
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-05-02 18:02:15 +0530

    Create cbse_result.php

---
+++ cbse_result.php
@@ -0,0 +1,54 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "ca-app-pub-3940256099942544/6300978111",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "ca-app-pub-3940256099942544/1033173712",
+      "priority": "1",
+      "counter": "3"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "ca-app-pub-3940256099942544/2247696110",
+      "priority": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Banner",
+      "placement_id": "1bd61e010479c716",
+      "priority": "0"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Interstitial",
+      "placement_id": "197f5ba608c27d44",
+      "priority": "0",
+      "counter": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Native",
+      "placement_id": "697c0a91e00756ca",
+      "priority": "0"
+    }
+  ],
+  "info": [
+    {
+      "url_type": "tenth",
+      "result_url": "http://google.com",
+      "udpates_url": "http://google.com"
+    },
+  {
+      "url_type": "twelth",
+      "result_url": "http://google.com",
+      "udpates_url": "http://google.com"
+    }
+  ]
+}
