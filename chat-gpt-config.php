1e3895f21e300c875845082938d977688352e7e1
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-05-05 22:30:53 +0530

    Create chat-gpt-config.php

---
+++ chat-gpt-config.php
@@ -0,0 +1,62 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Facebook",
+      "unit_type": "Banner",
+      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Facebook",
+      "unit_type": "Interstitial",
+      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "priority": "0",
+      "timer": "1500",
+      "counter": "0"
+    },
+    {
+      "ad_network": "Facebook",
+      "unit_type": "Native",
+      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "ca-app-pub-3940256099942544/6300978111",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "ca-app-pub-3940256099942544/1033173712",
+      "priority": "0",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "ca-app-pub-3940256099942544/2247696110",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Applovin",
+      "unit_type": "Banner",
+      "placement_id": "",
+      "priority": "0"
+    },
+    {
+      "ad_network": "Applovin",
+      "unit_type": "Interstitial",
+      "placement_id": "",
+      "priority": "0",
+      "counter": "0"
+    },
+    {
+      "ad_network": "Applovin",
+      "unit_type": "Native",
+      "placement_id": "",
+      "priority": "0"
+    }
+  ]
+}
