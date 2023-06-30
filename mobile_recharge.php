88f315ab09fe871db5439a53981f20f84909dc80
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-06-30 13:29:43 +0530

    Update mobile_recharge.php

--- mobile_recharge.php
+++ mobile_recharge.php
@@ -4,45 +4,45 @@
       "ad_network": "Admob",
       "unit_type": "Banner",
       "placement_id": "ca-app-pub-3940256099942544/6300978111",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Interstitial",
       "placement_id": "ca-app-pub-3940256099942544/1033173712",
-      "priority": "0",
-      "counter": null
+      "priority": "1",
+      "counter": "3"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Native",
       "placement_id": "ca-app-pub-3940256099942544/2247696110",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Appopen",
       "placement_id": "ca-app-pub-3940256099942544/3419835294",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Banner",
       "placement_id": "659579822681380_660146699291359",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Interstitial",
       "placement_id": "659579822681380_660146809291348",
-      "priority": "1",
+      "priority": "0",
       "counter": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Native",
       "placement_id": "659579822681380_660146905958005",
-      "priority": "1"
+      "priority": "0"
     }
   ],
   "other_info": [
