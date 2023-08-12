9b19ad3a04aa13d1768f83280f8323648e33c252
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-08-12 16:21:20 +0530

    Update test-ads.php

--- test-ads.php
+++ test-ads.php
@@ -4,66 +4,64 @@
       "ad_network": "Admob",
       "unit_type": "Banner",
       "placement_id": "ca-app-pub-3940256099942544/6300978111",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Interstitial",
       "placement_id": "ca-app-pub-3940256099942544/1033173712",
-      "priority": "1",
-      "counter": "5"
+      "priority": "0",
+      "counter": "2"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Native",
       "placement_id": "ca-app-pub-3940256099942544/2247696110",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Admob",
-      "unit_type": "RewardAd",
-      "placement_id": "ca-app-pub-3940256099942544/5224354917",
-      "priority": "1"
-    },
-    {
-      "ad_network": "Admob",
-      "unit_type": "AppOpenAd",
+      "unit_type": "Appopen",
       "placement_id": "ca-app-pub-3940256099942544/3419835294",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "AppLovin",
       "unit_type": "Banner",
-      "placement_id": "34889faef11d011b",
+      "placement_id": "",
       "priority": "0"
     },
     {
       "ad_network": "AppLovin",
       "unit_type": "Interstitial",
-      "placement_id": "e287a2365b538a7e",
+      "placement_id": "",
       "priority": "0",
-      "counter": "3"
+      "counter": "1"
     },
     {
       "ad_network": "AppLovin",
       "unit_type": "Native",
-      "placement_id": "5d48693a6853f74c",
+      "placement_id": "",
       "priority": "0"
-    }
-  ],
-  "info": [
+    },
     {
-      "type": "random_call_video",
-      "count": 50,
-      "is_show": "yes"
-    }
-  ],
-  "webrtc": [
+      "ad_network": "Meta",
+      "unit_type": "Banner",
+      "placement_id": "139088722530953_139095805863578",
+      "priority": "1"
+    },
     {
-      "provider": "agora",
-      "app_id": "8079664ca5fe4cb086b4dd11bd948176",
-      "certificate": "34b5846cbdf14a6e85d9ae4601e119ab",
-      "channel": "deepcall"
+      "ad_network": "Meta",
+      "unit_type": "Interstitial",
+      "placement_id": "139088722530953_139095942530231",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Meta",
+      "unit_type": "Native",
+      "placement_id": "139088722530953_139096065863552",
+      "priority": "1"
     }
   ]
 }
