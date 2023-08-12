60874cf70f355de0d6b24aa061c734f041662bcf
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-08-12 16:22:09 +0530

    Update test-ads.php

--- test-ads.php
+++ test-ads.php
@@ -4,26 +4,26 @@
       "ad_network": "Admob",
       "unit_type": "Banner",
       "placement_id": "ca-app-pub-3940256099942544/6300978111",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Interstitial",
       "placement_id": "ca-app-pub-3940256099942544/1033173712",
-      "priority": "0",
-      "counter": "2"
+      "priority": "1",
+      "counter": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Native",
       "placement_id": "ca-app-pub-3940256099942544/2247696110",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Appopen",
       "placement_id": "ca-app-pub-3940256099942544/3419835294",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "AppLovin",
@@ -48,20 +48,20 @@
       "ad_network": "Meta",
       "unit_type": "Banner",
       "placement_id": "139088722530953_139095805863578",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Interstitial",
       "placement_id": "139088722530953_139095942530231",
       "priority": "1",
-      "counter": "1"
+      "counter": "0"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Native",
       "placement_id": "139088722530953_139096065863552",
-      "priority": "1"
+      "priority": "0"
     }
   ]
 }
