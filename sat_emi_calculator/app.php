2b687460894c9f054d4ec06da6e7ad4c05693eb5
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2024-01-15 16:55:56 +0530

    Update app.php

--- sat_emi_calculator/app.php
+++ sat_emi_calculator/app.php
@@ -4,51 +4,51 @@
       "ad_network": "Admob",
       "unit_type": "Banner",
       "placement_id": "ca-app-pub-3940256099942544/6300978111",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Interstitial",
       "placement_id": "ca-app-pub-3940256099942544/1033173712",
-      "priority": "0",
+      "priority": "1",
       "counter": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Native",
       "placement_id": "ca-app-pub-3940256099942544/2247696110",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "RewardAd",
       "placement_id": "ca-app-pub-3940256099942544/5224354917",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Admob",
       "unit_type": "Appopen",
       "placement_id": "ca-app-pub-3940256099942544/3419835294",
-      "priority": "0"
+      "priority": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Banner",
       "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Interstitial",
       "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-      "priority": "1",
+      "priority": "0",
       "counter": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Native",
       "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-      "priority": "1"
+      "priority": "0"
     },
     {
       "ad_network": "AppLovin",
