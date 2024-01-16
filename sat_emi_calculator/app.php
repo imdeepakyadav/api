cfa47d3718a4ca878b80412bf325367f8755b320
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2024-01-16 16:35:37 +0530

    Update app.php

--- sat_emi_calculator/app.php
+++ sat_emi_calculator/app.php
@@ -34,20 +34,20 @@
     {
       "ad_network": "Meta",
       "unit_type": "Banner",
-      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "placement_id": "780522353408811_780526990075014",
       "priority": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Interstitial",
-      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "placement_id": "780522353408811_780527120075001",
       "priority": "1",
       "counter": "1"
     },
     {
       "ad_network": "Meta",
       "unit_type": "Native",
-      "placement_id": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+      "placement_id": "780522353408811_780527313408315",
       "priority": "1"
     },
     {
