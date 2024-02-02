c71f1ddf449b266e8e5e90dea5f02436bc636303
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2024-02-02 16:20:26 +0530

    Update test-ads.php

--- test-ads.php
+++ test-ads.php
@@ -1,9 +1,7 @@
-[
-  {
-    "id": "1",
-    "banner_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-    "interstitial_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-    "native_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
-    "ads": "meta"
-  }
-]
+{
+  "id": "1",
+  "banner_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+  "interstitial_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+  "native_ad": "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+  "ads": "meta"
+}
