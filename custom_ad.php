7895fc88b610ea528b0e2a8b591343cba817a906
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2024-02-19 17:05:00 +0530

    Create custom_ad.php

---
+++ custom_ad.php
@@ -0,0 +1,18 @@
+{"custom_ad": [
+    {
+      "type": "Banner",
+      "title": "YOU ARE USING OLD VERSION\nWATCH ALL LIVE LEAGUES\nPlease install our new app now to watch all football match live stream free.",
+      "ad_url": "https://bit.ly/48jXpr9",
+      "img_url": "https://i.postimg.cc/3Nq8F3rL/Untitled111.png",
+      "show": "0"
+    },
+    {
+      "type": "Interstitial",
+      "title": "YOU ARE USING OLD VERSION\nWATCH ALL LIVE LEAGUES\nPlease install our new app now to watch all football match live stream free.",
+      "ad_url": "https://bit.ly/48jXpr9",
+      "img_url": "https://i.postimg.cc/3Nq8F3rL/Untitled111.png",
+      "btn_text": "Update App",
+      "show": "1",
+      "enter_app": "1"
+    }
+  ]}
